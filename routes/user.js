2cf65074f843c08a77f73998725b9414878ae291
eki <eki1490@gmail.com>
2021-04-07 19:49:06 +0700

    create jwt helper

--- routes/user.js
+++ routes/user.js
@@ -18,6 +18,45 @@ router.post('/',
         if (!errors.isEmpty) {
             return res.status(400).json({ errors: errors.array() })
         }
+
+        const { name, email, password } = req.body
+
+        try {
+            let user = User.findOne({ email })
+            if (user) {
+                return res.status(400).json({
+                    msg: 'User already exists'
+                })
+            }
+
+            user = new User({
+                name,
+                email,
+                password
+            })
+
+            const salt = await bcrypt.genSalt(10)
+            user.password = await bcrypt.hash(password, salt)
+            await user.save()
+
+            const payload = {
+                user: {
+                    id :user.id
+                }
+            }
+
+            jwt.sign( payload, process.env.JWT_SECRET, 
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) {
+                        throw err
+                    }
+                    res.json({ token })
+                })
+        } catch (error) {
+            console.error(err.message)
+            res.status(500).send('Server error')
+        }
 })
 
 module.export = router
