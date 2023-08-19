db114e726a05d7a5d6ffbc3b5d85c474e7c1950a
Teheskhiel Fritz <60809044+maro14@users.noreply.github.com>
2023-08-19 01:55:08 +0000

    update the docker and index

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,20 @@
+# Use an official Node.js runtime as the base image
 FROM node:14
 
-WORKDIR /app
+# Set the working directory inside the container
+WORKDIR /usr/src/app
 
+# Copy package.json and yarn.lock to the working directory
+COPY package*.json yarn.lock ./
+
+# Install project dependencies using Yarn
 RUN yarn install
 
-CMD ["yarn", "start"]
+# Copy the rest of the application files to the working directory
+COPY . .
 
-EXPOSE 3000
+# Expose the port your Express app is listening on
+EXPOSE 5000
+
+# Define the command to run your app
+CMD ["yarn", "start"]
--- index.js
+++ index.js
@@ -2,42 +2,42 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
+const app = express();
 
-//env variable
+// Load environment variables
 require("dotenv").config();
 
-const app = express();
-app.use(express.json());
+// Middleware
 app.use(morgan("dev"));
 app.use(cors());
 app.use(helmet());
+app.use(express.json());
 
+// Root endpoint
 app.get("/", (req, res) => {
   res.json({
-    msg: "OK",
+    message: "Server is running",
   });
 });
 
-//Database connection
+// Connect to the database
 const dbConnect = require("./database/mongodb");
 
-//Route api
+// Routers
 const userRouter = require("./routes/user");
 const roomRouter = require("./routes/room");
 const bookRouter = require("./routes/booking");
-const authRouter = require('./routes/auth')
+const authRouter = require("./routes/auth");
 
+// Use routers
 app.use("/user", userRouter);
 app.use("/room", roomRouter);
 app.use("/book", bookRouter);
-app.use('/auth', authRouter)
+app.use("/auth", authRouter);
 
-
+// Set port and start the server
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
-
-    console.log(`Server on ${PORT}`);
-    dbConnect();
-})
-
+  console.log(`Server is running on port ${PORT}`);
+  dbConnect();
+});
